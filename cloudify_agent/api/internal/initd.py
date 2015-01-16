8fb2b4894024cec5c265cc9c1f11d30d7c381066
iliapolo <elip@gigaspaces.com>
2015-01-17 00:01:02 +0200

    add debug log

--- cloudify_agent/api/internal/initd.py
+++ cloudify_agent/api/internal/initd.py
@@ -258,7 +258,8 @@ class GenericLinuxDaemon(Daemon):
             resource_path='disable-requiretty.sh'
         )
         self._run('chmod +x {0}'.format(disable_requiretty_script_path))
-        self._run('cat {0}'.format(disable_requiretty_script_path))
+        result = self._run('cat {0}'.format(disable_requiretty_script_path))
+        self.logger.info(result.std_out)
         self._run('sudo {0}'.format(disable_requiretty_script_path))
 
     def _verify_no_celery_error(self):
